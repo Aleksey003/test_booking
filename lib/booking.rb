9af6b75a0c3bf94f87fc85e1b2f1ecf7797fd2a7
Aleksey Lks <alekseylks@gmail.com>
2016-07-21 16:21:26 +0300

    added logout and Active properties scraping

--- lib/booking.rb
+++ lib/booking.rb
@@ -9,9 +9,21 @@ class Booking
     session.fill_in 'loginname', with: '1342069'
     session.fill_in 'password', with: 'testing987!'
     session.find('.btn-primary').click
-    session.all('.hotel_row tr').each do |tr|
-      puts tr.text
+
+    table_results = session.all('#group_login_overview td').map do |row|
+      row.text 
     end
-    #session.save_and_open_page
+    p table_results
+  end
+
+  def self.logout
+    session = Capybara::Session.new(:webkit)
+    session.visit('https://admin.booking.com/')
+    session.visit('https://admin.booking.com/')
+    session.fill_in 'loginname', with: '1342069'
+    session.fill_in 'password', with: 'testing987!'
+    session.find('.btn-primary').click
+    session.find('.logout').click
+    p session.find('.login-form-title').text
   end
 end
